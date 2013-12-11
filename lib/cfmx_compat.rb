68a6ab7a58a3a2aa4e9ecc02153d059fd61465bb
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-11 18:57:31 +0000

    Convert strings

--- lib/cfmx_compat.rb
+++ lib/cfmx_compat.rb
@@ -1,11 +1,11 @@
-require "worker"
+require 'worker'
 
 class CfmxCompat
-  def self.encrypt(plaintext, key, encoding = "uu")
+  def self.encrypt(plaintext, key, encoding = 'uu')
     Worker.new(encoding, key).encrypt(plaintext)
   end
 
-  def self.decrypt(ciphertext, key, encoding = "uu")
+  def self.decrypt(ciphertext, key, encoding = 'uu')
     Worker.new(encoding, key).decrypt(ciphertext)
   end
 end
