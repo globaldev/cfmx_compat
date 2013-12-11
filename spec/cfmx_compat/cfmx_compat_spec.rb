12e10d552cfd4a0c3053b23cdf4848b70a90c4ae
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-11 23:16:39 +0000

    Convert strings

--- spec/cfmx_compat/cfmx_compat_spec.rb
+++ spec/cfmx_compat/cfmx_compat_spec.rb
@@ -12,9 +12,9 @@ describe CfmxCompat do
     it { expect { CfmxCompat.encrypt(plaintext, '') }.to raise_error(ArgumentError) }
 
     context "encoding" do
-      specify { CfmxCompat.encrypt(plaintext, key, "uu").should == "*<@>J&XG+`99/40``\n" }
-      specify { CfmxCompat.encrypt(plaintext, key, "hex").should == "7207AA1B89CB01964F51" }
-      specify { CfmxCompat.encrypt(plaintext, key, "base64").should == "cgeqG4nLAZZPUQ==" }
+      specify { CfmxCompat.encrypt(plaintext, key, 'uu').should == "*<@>J&XG+`99/40``\n" }
+      specify { CfmxCompat.encrypt(plaintext, key, 'hex').should == "7207AA1B89CB01964F51" }
+      specify { CfmxCompat.encrypt(plaintext, key, 'base64').should == "cgeqG4nLAZZPUQ==" }
       specify { CfmxCompat.encrypt(plaintext, key).should == CfmxCompat.encrypt(plaintext, key, 'uu') }
     end
   end
