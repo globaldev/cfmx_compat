9f22625b6e23c533263bd4bb2ae2f48cb3b2ad46
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-11 15:07:07 +0000

    Rename

--- spec/cfmx_compat/cfmx_compat_spec.rb
+++ spec/cfmx_compat/cfmx_compat_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe CfmxCompat do
   let(:key) { "mySecretKey" }
-  let(:plain) { "clear text" }
+  let(:plaintext) { "clear text" }
 
   describe ".encrypt" do
     context "nil string value" do
@@ -13,32 +13,32 @@ describe CfmxCompat do
 
     context "nil key value" do
       it "raises ArgumentError" do
-        expect { CfmxCompat.encrypt(plain, nil) }.to raise_error(ArgumentError)
+        expect { CfmxCompat.encrypt(plaintext, nil) }.to raise_error(ArgumentError)
       end
     end
 
     context "encoding" do
       context "when not specified" do
         it "encrypts correctly" do
-          expect(CfmxCompat.encrypt(plain, key)).to eq "*<@>J&XG+`99/40``\n"
+          expect(CfmxCompat.encrypt(plaintext, key)).to eq "*<@>J&XG+`99/40``\n"
         end
       end
 
       context "when uu" do
         it "encrypts correctly" do
-          expect(CfmxCompat.encrypt(plain, key, "uu")).to eq "*<@>J&XG+`99/40``\n"
+          expect(CfmxCompat.encrypt(plaintext, key, "uu")).to eq "*<@>J&XG+`99/40``\n"
         end
       end
 
       context "when hex" do
         it "encrypts correctly" do
-          expect(CfmxCompat.encrypt(plain, key, "hex")).to eq "7207AA1B89CB01964F51"
+          expect(CfmxCompat.encrypt(plaintext, key, "hex")).to eq "7207AA1B89CB01964F51"
         end
       end
 
       context "when base64" do
         it "encrypts correctly" do
-          expect(CfmxCompat.encrypt(plain, key, "base64")).to eq "cgeqG4nLAZZPUQ=="
+          expect(CfmxCompat.encrypt(plaintext, key, "base64")).to eq "cgeqG4nLAZZPUQ=="
         end
       end
     end
@@ -53,32 +53,32 @@ describe CfmxCompat do
 
     context "nil key value" do
       it "raises ArgumentError" do
-        expect { CfmxCompat.decrypt(plain, nil) }.to raise_error(ArgumentError)
+        expect { CfmxCompat.decrypt(plaintext, nil) }.to raise_error(ArgumentError)
       end
     end
 
     context "encoding" do
       context "when not specified" do
         it "decrypts correctly" do
-          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plain, key), key)).to eq plain
+          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plaintext, key), key)).to eq plaintext
         end
       end
 
       context "when uu" do
         it "decrypts correctly" do
-          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plain, key, :uu), key, :uu)).to eq plain
+          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plaintext, key, :uu), key, :uu)).to eq plaintext
         end
       end
 
       context "when hex" do
         it "decrypts correctly" do
-          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plain, key, :hex), key, :hex)).to eq plain
+          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plaintext, key, :hex), key, :hex)).to eq plaintext
         end
       end
 
       context "when base64" do
         it "decrypts correctly" do
-          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plain, key, :base64), key, :base64)).to eq plain
+          expect(CfmxCompat.decrypt(CfmxCompat.encrypt(plaintext, key, :base64), key, :base64)).to eq plaintext
         end
       end
     end
